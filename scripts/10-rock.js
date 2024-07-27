f394c8af6fa3550bc2f2ee6544ff9b3058bfbf49
santhoshpandy <1012santhosh@gmail.com>
2024-07-27 21:23:37 +0530

    first commit

---
+++ scripts/10-rock.js
@@ -0,0 +1,102 @@
+
+let score = JSON.parse(localStorage.getItem('score'));
+
+if(!score){
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };
+};
+
+updateScoreElement();
+
+let computerMove = '';
+
+function pickcomputerMove(){
+const randomNumber = Math.random();
+let computerMove= '';
+if(randomNumber >= 0 && randomNumber <= 1/3){
+    computerMove = 'rock';
+}
+else if(randomNumber >= 1/3 && randomNumber <= 2/3){
+    computerMove = 'paper';
+}
+else if(randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+}
+return computerMove;
+
+}
+
+function playGame(playerMove){
+    const computerMove = pickcomputerMove();
+
+    let result ='';
+
+if(playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+        result = 'You lose'
+    }
+    else if(computerMove === 'paper'){
+        result = 'You win';
+    }
+    else if(computerMove === 'scissors'){
+        result = 'Tie';
+    }
+}
+
+else if(playerMove === 'paper'){
+
+    if(computerMove === 'rock'){
+        result = 'You win'
+    }
+    else if(computerMove === 'paper'){
+        result = 'Tie';
+    }
+    else if(computerMove === 'scissors'){
+        result = 'You lose';
+    }
+}
+
+else if(playerMove === 'rock'){
+ 
+    if(computerMove === 'rock'){
+        result = 'Tie'
+    }
+    else if(computerMove === 'paper'){
+        result = 'You lose';
+    }
+
+    else if(computerMove === 'scissors'){
+        result = 'You win';
+    }
+}
+
+if(result === 'You win'){
+    score.wins += 1;
+}
+else if(result === 'You lose'){
+    score.losses +=1;
+}
+else if(result === 'Tie'){
+    score.ties +=1;
+}
+
+localStorage.setItem('score', JSON.stringify(score));
+updateScoreElement();
+document.querySelector('.js-result').innerHTML = result;
+document.querySelector('.js-moves').innerHTML = `You <img src="assets/${playerMove}-emoji.png" alt="rock-emoji" class="w-12 h-12 bg-transparent  ">
+<img src="assets/${computerMove}-emoji.png" alt="scissors-emoji" class="w-12 h-12 bg-transparent  ">
+Computer`;
+
+}
+
+function updateScoreElement(){
+document.querySelector('.js-score')
+.innerHTML =`Wins: ${score.wins}, 
+Losses: ${score.losses}, Ties: ${score.ties}` ;
+}
+
+
+
